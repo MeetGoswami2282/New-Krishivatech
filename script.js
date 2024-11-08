e29e484764817700e916e329362a3a12e1f11283
MeetGoswami2282 <meetgoswami842@gmail.com>
2024-11-08 21:11:45 +0530

    Intitial Commit

---
+++ script.js
@@ -0,0 +1,73 @@
+// MILESTONE NUMBER
+
+document.addEventListener('DOMContentLoaded', () => {
+  const counters = document.querySelectorAll('.count');
+  const speed = 100; // Speed of counting
+
+  counters.forEach(counter => {
+      const updateCount = () => {
+          const target = +counter.getAttribute('data-count');
+          const count = +counter.innerText;
+
+          const increment = target / speed;
+
+          if (count < target) {
+              counter.innerText = Math.ceil(count + increment);
+              setTimeout(updateCount, 50);
+          } else {
+              counter.innerText = target;
+          }
+      };
+
+      updateCount();
+  });
+});
+
+
+
+
+
+var sliderCounter = 0;
+var sliderContent = [
+  "Web Development",
+  "WordPress Development",
+  "App Development",
+  "Plugin Development",
+  "CRM Integrations"
+];
+var slider = document.querySelector("#slider");
+var sliderValue = document.querySelector("#sliderValue");
+
+function slide() {
+  if (sliderCounter >= sliderContent.length) {
+    sliderCounter = 0;
+  }
+
+  sliderValue.innerHTML = "";
+  
+  sliderValue.classList.remove("holder-animation");
+  void sliderValue.offsetWidth;
+  sliderValue.classList.add("holder-animation");
+  
+  for (i = 0; i < sliderContent[sliderCounter].length; i++) {
+    let letterDiv = document.createElement("div");
+    letterDiv.innerHTML = sliderContent[sliderCounter][i];
+
+    if (letterDiv.innerHTML == " ") {
+      letterDiv.innerHTML = "&nbsp;";
+    }
+    letterDiv.classList.add("start");
+    letterDiv.classList.add("animation");
+    letterDiv.style.animationDelay = i / 10 + "s";
+    sliderValue.appendChild(letterDiv);
+  }
+
+  sliderCounter++;
+}
+
+slide();
+setInterval(slide, 4000);
+
+
+
+
