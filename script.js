8e563bbdf8131576c4068aae85da963b9eb31016
MeetGoswami2282 <meetgoswami842@gmail.com>
2024-11-10 19:41:29 +0530

    hello

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+
+
+const images = [...document.querySelectorAll('.slider-item-img')];
+const slider = document.querySelector('.slider');
+let sliderWidth;
+let imageWidth;
+let current = 0;
+let target = 0;
+let ease = 0.05; // smaller = slower
+
+// init img src
+images.forEach((img, i)=> {
+  img.style.backgroundImage = `url('${img.dataset.img}')`;
+});
+
+// linear interpolation
+const lerp = (start, end, t) => {
+  return start * (1-t) + end * t;
+}
+
+const setTransform = (el, transform) => {
+  el.style.transform = transform;
+}
+
+const initSlider = () => {
+  sliderWidth = slider.getBoundingClientRect().width;
+  imageWidth = sliderWidth / images.length;
+  document.body.style.height = `${sliderWidth - (window.innerWidth - window.innerHeight)}px`
+}
+
+const animateImages = () => {
+  let ratio = current / imageWidth;
+  let intersectionRatioValue;
+  
+  images.forEach((img, i) => {
+    intersectionRatioValue = ratio - (i * 0.15);
+    setTransform(img, `translateX(${intersectionRatioValue * 20}px)`)
+  })
+}
+
+const animate = () => {
+  current = parseFloat(lerp(current, target, ease)).toFixed(2);
+  target = window.scrollY;
+  setTransform(slider, `translateX(-${current}px)`);
+  animateImages();
+  requestAnimationFrame(animate);
+}
+
+initSlider();
+animate();
+
 // MILESTONE NUMBER
 
 document.addEventListener('DOMContentLoaded', () => {
